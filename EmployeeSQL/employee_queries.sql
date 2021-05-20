2a1e24528b0d0fa36a0fb5df4bf0aeaa924d1ce5
Krista Joy <krista_joy@hotmail.com>
2021-05-20 08:30:45 -0700

    updated code

--- EmployeeSQL/employee_queries.sql
+++ EmployeeSQL/employee_queries.sql
@@ -1,14 +1,15 @@
 -- List the following details of each employee: employee number, last name, first name, sex, and salary.
+
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees AS e
 JOIN salaries AS s ON
 e.emp_no = s.emp_no
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
+
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE hire_date LIKE '1986%'
---WHERE year(hire_date) = 1986
+WHERE EXTRACT(YEAR FROM CAST (hire_date AS TIMESTAMP)) = '1986';
 
 /* List the manager of each department with the following information: 
 department number, department name, the manager's employee number, last name, first name.*/
@@ -18,7 +19,8 @@ FROM dept_manager AS dm
 JOIN departments AS d ON
 dm.dept_no = d.dept_no
 JOIN employees AS e ON
-e.emp_no = dm.emp_no;
+e.emp_no = dm.emp_no
+WHERE emp_title_id = 'm0001'
 
 /* List the department of each employee with the following information: 
 employee number, last name, first name, and department name.*/
